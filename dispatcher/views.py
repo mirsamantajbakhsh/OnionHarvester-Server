13dc7a186ae25051bc0304b95edb5aaf40b02eb8
mohammad <w.mohammad.k@gmail.com>
2017-12-24 16:10:40 +0330

    simplified address generation

--- dispatcher/views.py
+++ dispatcher/views.py
@@ -2,10 +2,11 @@ import datetime
 
 import json
 
-import itertools
 from django.shortcuts import render
 from django.http import HttpResponse
-from dispatcher.models import Pool, Response
+import uuid
+
+from dispatcher.models import Pool
 
 
 def index(request):
@@ -32,54 +33,75 @@ def index(request):
 
 
 def generate(request):
+    client_id = uuid.uuid4().hex
     ports = ['80', '443']
-    last_address = 'aaaaaaaaaaaaaaaa'
+    last_address = '7777777777777777'
     # 128 addresses in range
     range_limit = 128
     # In minutes
     timeout = 30
-    last_range = Pool.objects.order_by('-id')[0]
+    dis_time = datetime.datetime.now(datetime.timezone.utc)
+    last_range = Pool.objects.order_by('-id')
     if not last_range:
-        address_range = domain_generator(last_address, range_limit)
+        address_range = __address_generator(last_address, range_limit)
     else:
-        timed_out_range = get_timed_out(timeout)
+        timed_out_range = __get_timed_out(timeout)
         if timed_out_range:
-            last_address = timed_out_range.address_range_end
+            last_address = timed_out_range[0].address_range_end
+            address_range = {
+                'start': timed_out_range[0].address_range_start,
+                'end': timed_out_range[0].address_range_end
+            }
         else:
-            last_address = last_range.address_range_end
-        address_range = domain_generator(last_address, range_limit)
+            last_address = last_range[0].address_range_end
+            address_range = __address_generator(last_address, range_limit)
     params = {
-        'start': address_range['start'],
-        'end': address_range['end'],
+        'id': client_id,
+        'start': ''.join(address_range['start']),
+        'end': ''.join(address_range['end']),
         'ports': ports,
         'timeout': timeout * 1000
     }
-    return HttpResponse(json.dumps(params), content_type="application/json")
+    new_pool = Pool()
+    new_pool.client_id = params['id']
+    new_pool.address_range_start = params['start']
+    new_pool.address_range_end = params['end']
+    new_pool.dis_time = dis_time
+    try:
+        Pool.objects.create(client_id=params['id'], address_range_start=params['start'],
+                            address_range_end=params['end'], dis_time=dis_time)
+        response = json.dumps(params)
+    except Exception as e:
+        response = "Some thing is wrong! Please try again later. Error:" + e.__str__()
+    return HttpResponse(response, content_type="application/json")
 
 
-def domain_generator(last_domain):
-    chars_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-             'v', 'w', 'x', 'y', 'z', '2', '3', '4', '5', '6', '7']
-    chars = list(last_domain)
-    for i, e in reversed(list(enumerate(chars))):
+def __address_generator(last_address, range_limit):
+    start_address = __next_address_generator(last_address)
+    end_address = last_address
+    for i in range(range_limit):
+        end_address = __next_address_generator(end_address)
+    address_range = {
+        'start': start_address,
+        'end': end_address,
+    }
+    return address_range
+
+
+def __next_address_generator(last_address):
+    last_address = list(last_address)
+    chars_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                  'u', 'v', 'w', 'x', 'y', 'z', '2', '3', '4', '5', '6', '7']
+    for i, e in reversed(list(enumerate(last_address))):
         if e == '7':
-            chars[i] = 'a'
+            last_address[i] = 'a'
         else:
-            index = i
-            char_index = chars_list.index(e)
+            last_address[i] = chars_list[chars_list.index(e) + 1]
             break
-    next_multi_pre_start_char = char_index + 1
-    chars[index] = chars_list[next_multi_pre_start_char]
-    end_chars = chars
-    print(''.join(chars))
-    next_pre_end_char = chars_list.index(chars[14]) + 3
-    end_chars[14] = chars_list[next_pre_end_char]
-    end_chars[15] = '7'
-    print(''.join(end_chars))
+    return last_address
 
 
-def get_timed_out(timeout):
+def __get_timed_out(timeout):
     time_threshold = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=timeout)
-    result = Pool.objects.filter(dis_time__lt=time_threshold)[0]
+    result = Pool.objects.filter(dis_time__lt=time_threshold)
     return result
-
